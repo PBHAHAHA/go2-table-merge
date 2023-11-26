60590dc7e49ca5d466239d92640317a03b26084a
PBHAHAHA <314709923@qq.com>
2023-11-26 10:52:42 +0800

    feat: 合并方法（列合并）

---
+++ src/getSpanArr.js
@@ -0,0 +1,23 @@
+/**
+ * 根据数据源和 param 计算出对应的 span 数组
+ * @param {*} param0  
+ * @returns 
+ */
+const getSpanArr = ({param ,data}) => {
+  const arr = []
+  let pos = 0
+  for (let i = 0; i < data.length; i++) {
+    if (i === 0) {
+      arr.push(1)
+      pos = 0
+    } else if (data[i][param] === data[i - 1][param]) {
+      // 判断当前元素与上一个元素是否相同
+      arr[pos] += 1
+      arr.push(0)
+    } else {
+      arr.push(1)
+      pos = i
+    }
+  }
+  return arr
+}
--- src/index.js
+++ src/index.js
@@ -1,51 +1,38 @@
-// let param = "name"
-// const getSpanArr = ({param ,data}) => {
-//   const arr = []
-//   let pos = 0
-//   for (let i = 0; i < data.length; i++) {
-//     if (i === 0) {
-//       arr.push(1)
-//       pos = 0
-//     } else if (data[i][param] === data[i - 1][param]) {
-//       // 判断当前元素与上一个元素是否相同
-//       arr[pos] += 1
-//       arr.push(0)
-//     } else {
-//       arr.push(1)
-//       pos = i
-//     }
-//   }
-//   return arr
-// }
-// var colIndex = [1]
-// const indexArr = ref([])
-// const objectSpanMethod = (e,{
-//   data, rule
-// }) => {
-//   console.log(rule)
-//   indexArr.value = rule
-//   if(rule.length == 0){
-//     return
-//   }
-//   console.log(e, indexArr)
-//   const {
-//     row,
-//     column,
-//     rowIndex,
-//     columnIndex,
-//   } = e
-//   // console.log(row)
-//   let obj = rule.filter(item => item.index == columnIndex)[0]
-//   if(obj){
-//     const span = getSpanArr({
-//       param: obj.name,
-//       data,
-//     })
-//     const r = span[rowIndex]
-//     const l = span[rowIndex] > 0 ? 1 : 0
-//     return {
-//       rowspan: r,
-//       colspan: l,
-//     }
-//   }
-// }
+import {
+  getSpanArr
+} from './getSpanArr'
+/**
+ * 
+ * @param {*} e element plus表格合并的默认参数
+ * @param {*} param1（data, rule） data: 表格的所有数据，rule: 表格合并的规则 例如：[{index: 0, name: 'name'}] 
+ * index 代表合并的列索引，name 代表合并的列名（也就是决定根据什么值决定合并）
+ * @returns 
+ */
+export const objectSpanMethod = (e,{
+  data, rule
+}) => {
+  indexArr = rule
+  if(rule.length == 0){
+    return
+  }
+  const {
+    row,
+    column,
+    rowIndex,
+    columnIndex,
+  } = e
+  // console.log(row)
+  let obj = rule.filter(item => item.index == columnIndex)[0]
+  if(obj){
+    const span = getSpanArr({
+      param: obj.name,
+      data,
+    })
+    const r = span[rowIndex]
+    const l = span[rowIndex] > 0 ? 1 : 0
+    return {
+      rowspan: r,
+      colspan: l,
+    }
+  }
+}
